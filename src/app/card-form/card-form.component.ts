1bd0394cc346a046a261242170b855ee0670cb6e
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2023-12-29 10:17:31 -0500

    Add DateFormControl class

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFromControl } from '../shared/model/date-from-control';
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
@@ -8,10 +9,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    cardNumber: new FormControl('', [Validators.required]),
-    expiration: new FormControl('', [Validators.required]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new DateFromControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
     securityCode: new FormControl('', [Validators.required]),
   });
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.cardForm.value);
+  }
 }
---
+++ src/app/shared/model/date-from-control.ts
@@ -0,0 +1,30 @@
+import { FormControl } from '@angular/forms';
+
+export class DateFromControl extends FormControl {
+  // override the default setValue method in the FormControl class
+  override setValue(value: string, options: any): void {
+    // Haul the value update if it's not a number or /
+    // this.value is sotred in the FormControl object by default
+    if (value.match(/^\d|\/|\d$/gi)) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+    // Haul the update if the value lenght is greater than 5
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    // Remove the '/' when user delete the value.length to 3
+    if (this.value.length === 3 && value.length === 2) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    // Insert a '/' after the secord input
+    if (value.length === 2) {
+      super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+      return;
+    }
+  }
+}
